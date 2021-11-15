db3a4d87211731bb1eab921e0508e9f418ccef04
coopwhit <coopwhit@iu.edu>
2021-11-14 21:08:09 -0500

    Map Commit

---
+++ map.py
@@ -0,0 +1,81 @@
+class House:
+    def __init__(self, node):
+        self.address = node
+        self.adj = {}
+
+    def __str__(self):
+        return str(self.address) + ' adjacent: ' + str([x.address for x in self.adj])
+
+    def add_neighbor(self, neighbor, weight=0):
+        self.adj[neighbor] = weight
+
+    def get_connections(self):
+        return self.adj.keys()  
+
+    def get_address(self):
+        return self.address
+
+    def get_speed(self, neighbor):
+        return self.adj[neighbor]
+
+class Map:
+    def __init__(self):
+        self.vert_dict = {}
+        self.num_vertices = 0
+
+    def __iter__(self):
+        return iter(self.vert_dict.values())
+
+    def add_house(self, node):
+        self.num_vertices = self.num_vertices + 1
+        new_vertex = House(node)
+        self.vert_dict[node] = new_vertex
+        return new_vertex
+
+    def get_vertex(self, n):
+        if n in self.vert_dict:
+            return self.vert_dict[n]
+        else:
+            return None
+
+    def add_street(self, frm, to, cost = 0):
+        if frm not in self.vert_dict:
+            self.add_house(frm)
+        if to not in self.vert_dict:
+            self.add_house(to)
+
+        self.vert_dict[frm].add_neighbor(self.vert_dict[to], cost)
+        self.vert_dict[to].add_neighbor(self.vert_dict[frm], cost)
+
+    def get_vertices(self):
+        return self.vert_dict.keys()
+
+if __name__ == '__main__':
+
+    g = Map()
+
+    g.add_house('a')
+    g.add_house('b')
+    g.add_house('c')
+    g.add_house('d')
+    g.add_house('e')
+    g.add_house('f')
+
+    g.add_street('a', 'b', 7)  
+    g.add_street('a', 'c', 9)
+    g.add_street('a', 'f', 14)
+    g.add_street('b', 'c', 10)
+    g.add_street('b', 'd', 15)
+    g.add_street('c', 'd', 11)
+    g.add_street('c', 'f', 2)
+    g.add_street('d', 'e', 6)
+    g.add_street('e', 'f', 9)
+
+    for v in g:
+        for w in v.get_connections():
+            vid = v.get_address()
+            wid = w.get_address()
+            print( '( %s , %s, %3d)'  % ( vid, wid, v.get_speed(w)))
+
+    for v in g:
+        print ('g.vert_dict[%s]=%s' %(v.get_address(), g.vert_dict[v.get_address()]))
