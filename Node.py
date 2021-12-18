5e06293f22f11cd51c862926cf4cbd140804a6e0
rysidron <54722631+rysidron@users.noreply.github.com>
2021-12-17 21:08:02 -0500

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,10 +1,10 @@
-
 # Project Node Class
 
 # noinspection PyInitNewSignature
 class Node:
-    def __init__(self, name, highway, position, speed, parent, neighbors, g, h, f):
+    def __init__(self, name, value, highway, position, speed, parent, neighbors, g, h, f):
         self.name = name  # name of the node for printing purposes
+        self.value = value  # value of the name
         self.highway = highway  # boolean
         self.position = position  # 2 tuple position on map (used to calculate cost and euclidean dist.)
         self.speed = speed  # value for the speed limit to this node
